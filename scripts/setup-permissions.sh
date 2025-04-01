8449ef86901b79104a42ec4536a865eee88c6860
aleksey-savin <44152076+aleksey-savin@users.noreply.github.com>
2025-04-01 14:07:13 +1000

    fixed script

--- scripts/setup-permissions.sh
+++ scripts/setup-permissions.sh
@@ -7,20 +7,20 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 # Создаем необходимые директории
-mkdir -p /opt/backups/simple-finances_bot/pre-deploy
+mkdir -p /opt/backups/simple-finances-bot/pre-deploy
 mkdir -p /opt/scripts
 
 # Устанавливаем владельца
-chown -R github-runner:github-runner /opt/backups/simple-finances_bot
+chown -R github-runner:github-runner /opt/backups/simple-finances-bot
 chown -R github-runner:github-runner /opt/scripts
 
 # Устанавливаем права
-chmod -R 755 /opt/backups/simple-finances_bot
+chmod -R 755 /opt/backups/simple-finances-bot
 chmod -R 755 /opt/scripts
 
 # Проверяем результат
 echo "Checking permissions..."
-ls -la /opt/backups/simple-finances_bot
+ls -la /opt/backups/simple-finances-bot
 ls -la /opt/scripts
 
 echo "Done! The system is ready for deployments"
